815f16a7759fa46aa4ad789b5f80ba140f781b25
chaylaz <chayla0504@gmail.com>
2024-10-22 05:15:23 +0700

    Menambahkan Laporan Praktikum 5.

--- 05_Antarmuka_Pengguna2/UNGUIDED/praktikum_5/lib/main.dart
+++ 05_Antarmuka_Pengguna2/UNGUIDED/praktikum_5/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'wisata_banyumas_screen.dart'; // Pastikan kamu menyimpan file WisataBanyumasScreen dengan nama ini
-
+import 'wisata_banyumas_screen.dart'; 
 void main() {
   runApp(WisataBanyumasApp());
 }
@@ -9,7 +8,7 @@ class WisataBanyumasApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // Menghilangkan banner debug di pojok kanan atas
+      debugShowCheckedModeBanner: false,  
       title: 'Wisata Banyumas',
       theme: ThemeData(
         primarySwatch: Colors.teal,
