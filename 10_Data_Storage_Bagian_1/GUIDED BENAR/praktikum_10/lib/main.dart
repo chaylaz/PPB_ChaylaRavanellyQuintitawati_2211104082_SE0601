7f9afb857012a932ad1999e2a7c4dc77c6c66e0a
chaylaz <chayla0504@gmail.com>
2024-11-20 15:41:06 +0700

    Menambahkan Guided Praktikum_10.

--- 10_Data_Storage_Bagian_1/GUIDED BENAR/praktikum_10/lib/main.dart
+++ 10_Data_Storage_Bagian_1/GUIDED BENAR/praktikum_10/lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:praktikum_10/view/my_db_view.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+void main() {
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Praktikum Data Storage',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
       home: MyDatabaseView(),
     );
